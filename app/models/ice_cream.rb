92d8effd7064e33a22c7c237a7ba8dda8e6087fb
hansenjl <jenn.leigh.hansen@gmail.com>
2019-06-12 07:31:02 -0700

    Add custom validator

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -3,10 +3,18 @@ class IceCream < ApplicationRecord
   belongs_to :user #creator of it
   has_many :reviews
   has_many :users, through: :reviews #people who have reviewed it
-
+  validate :not_a_repeat
 
   def brand_attributes=(attributes)
-    brand = Brand.find_or_create_by(attributes) if !name.empty?
+    brand = Brand.find_or_create_by(attributes) if !attributes['name'].empty?
   end
+
+  def not_a_repeat
+    if IceCream.where(brand_id: brand_id, flavor: flavor).count > 0
+      errors.add(:flavor, "- this flavor has already been added")
+    end
+  end
+
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :ice_cream
-
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
 
